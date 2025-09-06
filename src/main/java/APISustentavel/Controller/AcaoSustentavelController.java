8eb62c3692184532a152a98954836665cf762731
Franciane SL <fran.schier@outlook.com>
2025-09-06 20:29:45 -0300

    feat(controller): adicionar AcaoSustentavelController com endpoint base

---
+++ src/main/java/APISustentavel/Controller/AcaoSustentavelController.java
@@ -0,0 +1,11 @@
+package APISustentavel.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/acoesSustentaveis")
+public class AcaoSustentavelController {
+
+
+}
