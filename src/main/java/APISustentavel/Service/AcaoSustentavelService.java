807f898d02e690ad24cfb5a5e9a29fdf87093320
Franciane SL <fran.schier@outlook.com>
2025-09-06 21:36:42 -0300

    feat(service): criar AcaoSustentavelService com CRUD completo

---
+++ src/main/java/APISustentavel/Service/AcaoSustentavelService.java
@@ -0,0 +1,44 @@
+package APISustentavel.Service;
+
+import APISustentavel.Model.Entity.AcaoSustentavel;
+import APISustentavel.Repository.AcaoSustentavelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AcaoSustentavelService {
+
+    @Autowired
+    AcaoSustentavelRepository acaoSustentavelRepository;
+
+    public List<AcaoSustentavel> findAllAcoesSustentaveis(){
+        return acaoSustentavelRepository.findAll();
+    }
+
+    public AcaoSustentavel findAcaoSustentavelById(Long id){
+        return acaoSustentavelRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("")
+        );
+    }
+
+    public AcaoSustentavel create(AcaoSustentavel acaoSustentavel){
+        return acaoSustentavelRepository.save(acaoSustentavel);
+    }
+
+    public AcaoSustentavel update(Long id, AcaoSustentavel acaoSustentavelUpdate) {
+        AcaoSustentavel existingAcaoSustentavel = findAcaoSustentavelById(id);
+        existingAcaoSustentavel.setTitulo(acaoSustentavelUpdate.getTitulo());
+        existingAcaoSustentavel.setDescricao(acaoSustentavelUpdate.getDescricao());
+        existingAcaoSustentavel.setCategoria(acaoSustentavelUpdate.getCategoria());
+        existingAcaoSustentavel.setDataRealizacao(acaoSustentavelUpdate.getDataRealizacao());
+        existingAcaoSustentavel.setResponsavel(acaoSustentavelUpdate.getResponsavel());
+        return acaoSustentavelRepository.save(existingAcaoSustentavel);
+    }
+
+    public void delete(Long id){
+        AcaoSustentavel acaoSustentavel = findAcaoSustentavelById(id);
+        acaoSustentavelRepository.delete(acaoSustentavel);
+    }
+}
