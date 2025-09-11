5954d8f63bc562bbbe82141a593ddb090c46049f
Franciane SL <fran.schier@outlook.com>
2025-09-11 20:48:19 -0300

    feat(repository): adicionar método findByCategoria para buscar ações sustentaveis por categoria.

--- src/main/java/APISustentavel/Repository/AcaoSustentavelRepository.java
+++ src/main/java/APISustentavel/Repository/AcaoSustentavelRepository.java
@@ -1,9 +1,15 @@
 package APISustentavel.Repository;
 
 import APISustentavel.Model.Entity.AcaoSustentavel;
+import APISustentavel.Model.Enum.CategoriaEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AcaoSustentavelRepository extends JpaRepository<AcaoSustentavel, Long> {
+
+    List<AcaoSustentavel> findByCategoria(CategoriaEnum categoria);
+
 }
